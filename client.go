379d13592e0dc2d53c177941be85ea3192f85a8d
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-07-13 17:58:47 +0900

    fix

--- client.go
+++ client.go
@@ -3,21 +3,38 @@ package deepl
 import (
 	"context"
 	"net/http"
+	"net/url"
+)
+
+type plan int32
+
+const (
+	Free plan = iota
+	Pro
 )
 
 type Client struct {
 	context.Context
-	header http.Header
+	header  http.Header
+	baseURL *url.URL
+	plan    plan
 }
 
-func New(accessToken string) *Client {
+func New(accessToken string, plan plan) *Client {
 	ctx := context.Background()
 	c := &Client{
 		Context: ctx,
 		header:  http.Header{},
+		plan:    plan,
 	}
 
-	c.header.Set("Authorization", "DeepL-Auth-Key "+accessToken)
+	if plan == Free {
+		c.baseURL, _ = url.Parse("https://api-free.deepl.com/v2/")
+	} else if plan == Pro {
+		c.baseURL, _ = url.Parse("https://api-pro.deepl.com/v2/")
+	}
+
+	c.header.Set("Authorization", "DeepL-Auth-Key " + accessToken)
 
 	return c
 }
