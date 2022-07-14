89d1d7dcb9b0cca6a517fda8e4960950620138d2
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-07-14 13:32:00 +0900

    errMessageの追加

--- error.go
+++ error.go
@@ -6,3 +6,8 @@ var (
 	ErrNilText       = errors.New("text should not nil")
 	ErrNilTargetLang = errors.New("text should not nil")
 )
+
+type ErrMessage struct {
+	Message string `json:"message"`
+	Detail  string `json:"detail"`
+}
